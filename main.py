a90c48815084d28dd748da71b5d41da84a943d46
ricardomongza <ricardomongza@gmail.com>
2022-11-08 16:25:29 +0100

    Finish practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from simulator import Simulator
-from simulator.data_generator import generate_table_b, generate_table_a, generate_table_c
+from simulator.data_generator import DataGeneratorEfficiencyChecker
 
 def main():
     simulator = Simulator()
@@ -12,5 +12,10 @@ def main():
     #simulator.run(15, [1, 5, 8, 10], table='a')
 
 
+def test_generators():
+    dgec = DataGeneratorEfficiencyChecker()
+    dgec.run()
+
+
 if __name__ == '__main__':
-    main()
+    test_generators()
--- simulator/data_generator.py
+++ simulator/data_generator.py
@@ -1,3 +1,5 @@
+import time, random
+from statistics import mean
 
 """
 Creates lookup table for uniform distribution
@@ -26,3 +28,69 @@ def generate_table_c(size):
     for i in range(int(size/2), size):
         table[i] = table[i-1] + (size - i) / max
     return table
+
+\
+
+
+
+
+
+
+
+
+class DataGeneratorEfficiencyChecker():
+
+    def __generate_demand(self, table):
+        u = random.random()
+        for i in range(len(table)):
+            if u < table[i]:
+                return i
+        return len(table) - 1
+
+    def __generate_demand_bin_search(self, table):
+        u = random.random()
+
+        left = 0
+        right = len(table) - 1
+
+        while (left < right):
+            mid = (left + right) // 2
+
+            if u == table[mid]:
+                return mid
+            elif u < table[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return left
+  
+    def run(self, iterations = 10_000):
+        
+        table = generate_table_a(100)
+
+        non_efficient_times = []
+        for _ in range(iterations):
+            start = time.time()
+            self.__generate_demand(table)
+            end = time.time()
+            non_efficient_times.append(end - start)
+
+        efficient_times = []
+        for _ in range(iterations):
+            start = time.time()
+            self.__generate_demand_bin_search(table)
+            end = time.time()
+            efficient_times.append(end - start)
+
+        print('Busqueda Binaria de Tabla C')
+        print(f'No eficiente: {mean(non_efficient_times):.20f}s')
+        print(f'Eficiente:    {mean(efficient_times):.20f}s')
+
+
+
+
+
+        
+
+
