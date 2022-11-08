c0f8f9415061f931d94edd6097d8a8bc3393db71
ricardomongza <ricardomongza@gmail.com>
2022-11-08 16:28:16 +0100

    Clean up

--- main.py
+++ main.py
@@ -3,19 +3,7 @@ from simulator.data_generator import DataGeneratorEfficiencyChecker
 
 def main():
     simulator = Simulator()
-
-    #simulator.run(5, [1, 5, 8, 10], table='a')
-    #simulator.run(8, [1, 5, 8, 10], table='a')
-    #simulator.run(10, [1, 5, 8, 10], table='a')
-    #simulator.run(10, [5], save_image=True)
-    #simulator.run_refunds(10, 5, [0, 10, 100, 1000], table='a', save_image=True)
-    #simulator.run(15, [1, 5, 8, 10], table='a')
-
-
-def test_generators():
-    dgec = DataGeneratorEfficiencyChecker()
-    dgec.run()
-
+    simulator.run(8, [1, 5, 8, 10], 'a')
 
 if __name__ == '__main__':
-    test_generators()
+    main()
--- simulator/data_generator.py
+++ simulator/data_generator.py
@@ -29,63 +29,55 @@ def generate_table_c(size):
         table[i] = table[i-1] + (size - i) / max
     return table
 
-\
 
+# class DataGeneratorEfficiencyChecker():
 
+#     def __generate_demand(self, table):
+#         u = random.random()
+#         for i in range(len(table)):
+#             if u < table[i]:
+#                 return i
+#         return len(table) - 1
 
+#     def __generate_demand_bin_search(self, table):
+#         u = random.random()
 
+#         left = 0
+#         right = len(table) - 1
 
+#         while (left < right):
+#             mid = (left + right) // 2
 
-
-
-class DataGeneratorEfficiencyChecker():
-
-    def __generate_demand(self, table):
-        u = random.random()
-        for i in range(len(table)):
-            if u < table[i]:
-                return i
-        return len(table) - 1
-
-    def __generate_demand_bin_search(self, table):
-        u = random.random()
-
-        left = 0
-        right = len(table) - 1
-
-        while (left < right):
-            mid = (left + right) // 2
-
-            if u == table[mid]:
-                return mid
-            elif u < table[mid]:
-                right = mid - 1
-            else:
-                left = mid + 1
+#             if u == table[mid]:
+#                 return mid
+#             elif u < table[mid]:
+#                 right = mid - 1
+#             else:
+#                 left = mid + 1
         
-        return left
+#         return left
   
-    def run(self, iterations = 10_000):
+#     def run(self, iterations = 10_000):
         
-        table = generate_table_a(100)
+#         table = generate_table_a(100)
 
-        non_efficient_times = []
-        for _ in range(iterations):
-            start = time.time()
-            self.__generate_demand(table)
-            end = time.time()
-            non_efficient_times.append(end - start)
+#         non_efficient_times = []
+#         for _ in range(iterations):
+#             start = time.time()
+#             self.__generate_demand(table)
+#             end = time.time()
+#             non_efficient_times.append(end - start)
 
-        efficient_times = []
-        for _ in range(iterations):
-            start = time.time()
-            self.__generate_demand_bin_search(table)
-            end = time.time()
-            efficient_times.append(end - start)
+#         efficient_times = []
+#         for _ in range(iterations):
+#             start = time.time()
+#             self.__generate_demand_bin_search(table)
+#             end = time.time()
+#             efficient_times.append(end - start)
 
-        print('Busqueda Binaria de Tabla C')
-        print(f'No eficiente: {mean(non_efficient_times):.20f}s')
-        print(f'Eficiente:    {mean(efficient_times):.20f}s')
+#         print('Busqueda Binaria de Tabla C')
+#         print(f'No eficiente: {mean(non_efficient_times):.20f}s')
+#         print(f'Eficiente:    {mean(efficient_times):.20f}s')
 
 
 
